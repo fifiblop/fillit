0c3e87d4fb9aab82cdf09aabdaaae245ef20e79d
Eliot PLUMECOCQ <eplumecocq.42@gmail.com>
2015-12-10 17:39:40 +0100

    update check pieces

--- ft_check_pieces.c
+++ ft_check_pieces.c
@@ -6,29 +6,35 @@
 /*   By: eplumeco <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 16:07:58 by eplumeco          #+#    #+#             */
-/*   Updated: 2015/12/09 19:49:16 by eplumeco         ###   ########.fr       */
+/*   Updated: 2015/12/10 16:36:38 by eplumeco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		check_valid_tab(char *str_piece)
+int		check_first_tab(char *str_piece)
 {
 	int		i;
-	int		count;
+	int		count_n;
+	int		size;
 
 	i = 0;
-	count = 0;
-	if (ft_strlen(str_piece) == 0)
+	count_n = 0;
+	size = ft_strlen(str_piece);
+	if (size == 0)
 		return (0);
-	while (str_piece[i])
+	while (i < size -1)
 	{
-		if (i % 5 == 4 && str_piece[i] == '\n')
-		count++;
-		if (count == 4 && (str_piece[i] != '\n'))
-		{
-			ft_putchar(str_piece[i]);
+		if (i % 5 == 4 && str_piece[i] != '\n')
 			return (0);
+		if (str_piece[i] == '\n')
+			count_n++;
+		if (count_n == 4)
+		{
+			count_n = 0;
+			if (str_piece[i + 1] != '\n')
+				return (0);
+			i++;
 		}
 		i++;
 	}
@@ -36,15 +42,37 @@ static int		check_valid_tab(char *str_piece)
 }
 
 
+/*int		check_all_tab(char *str_piece, int count)*/
+/*{*/
+	/*if ()*/
+/*}*/
+
+
 int		main(void)
 {
 	char	*test1;
 	char	*test2;
+	char	*test3;
+	char	*test4;
 
 	test1 = "...#\n...#\n...#\n...#\n";
-		ft_putnbr(check_valid_tab(test1));
+		ft_putendl("test pour 1 seule piece valide");
+		ft_putnbr(check_first_tab(test1));
 		ft_putchar('\n');
+
 	test2 = "...#\n...#\n...#\n...#\n\n....\n....\n....\n####\n";
-		ft_putnbr(check_valid_tab(test2));
-		return (0);
+		ft_putendl("test pour 2 pieces valides");
+		ft_putnbr(check_first_tab(test2));
+		ft_putchar('\n');
+
+	test3 = "...#\n...#\n...#\n...#\n\n....\n....\n....\n####\n\n.###\n...#\n....\n....\n\n....\n..##\n.##.\n....\n";
+		ft_putendl("test pour 4 pieces valides");
+		ft_putnbr(check_first_tab(test3));
+		ft_putchar('\n');
+
+	test4 = "...#\n...#\n....#\n...#\n\n....\n..\n#....#\n...#\n....\n....\n\n....\n..##\n.##.\n....\n";
+		ft_putendl("test pour 4 pieces non valides");
+		ft_putnbr(check_first_tab(test4));
+		ft_putchar('\n');
+	return (0);
 }
