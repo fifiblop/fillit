1bcf06f4f1f49a25a1a3304de0fdc6646ca5c64f
Philippe DELEFOSSE <pdelefos@e3r4p21.42.fr>
2015-12-11 14:36:24 +0100

    removing tests

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pdelefos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 15:50:53 by pdelefos          #+#    #+#             */
-/*   Updated: 2015/12/11 11:13:18 by pdelefos         ###   ########.fr       */
+/*   Updated: 2015/12/11 14:36:10 by pdelefos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 int		main(int ac, char **av)
 {
-	while (1)
-	{
-		if (ac == 2)
-			ft_putstr(ft_get_pieces(av[1]));
-	}
+	if (ac == 2)
+		ft_putstr(ft_get_pieces(av[1]));
 	return (0);
 }
